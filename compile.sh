e187060008ccce2658fee289246dc837d69e3129
Brad Caron <bacaron@iu.edu>
2020-05-25 12:35:40 -0400

    Reason: add thal lat post

--- compile.sh
+++ compile.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 module load matlab/2017a
 
-mkdir compiled
+mkdir compiled planeROI
 
 cat > build.m <<END
-addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/bacaron/git/wma_tools'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/spm'))
-addpath(genpath('/N/u/brlife/git/wma_tools'))
+addpath(genpath('/N/u/brlife/git/mba'))
+addpath(genpath('/N/u/hayashis/git/vistasoft'))
 mcc -m -R -nodisplay -d compiled classificationGenerator
+mcc -m -R -nodisplay -d planeROI generatePlaneROI
 exit
 END
 matlab -nodisplay -nosplash -r build
